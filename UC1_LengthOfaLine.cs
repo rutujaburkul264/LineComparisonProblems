ea05fd02ec19774c899ff12c94186c9b70bb7eb5
rutujaburkul264 <rutujaburkul264@gmail.com>
2021-09-14 22:56:54 +0530

    UC1-LengthOfLine

--- UC1_LengthOfaLine.cs
+++ UC1_LengthOfaLine.cs
@@ -1,1 +1,22 @@
-"Welcome to Line Comparison Computation."
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparisonproblems
+{
+    class UC1_LengthOfaLine
+    {
+        public static void LengthOfALine()
+        {
+            Console.WriteLine("Enter x1 & y1 co-ordinates");
+            var x1 = Convert.ToDouble(Console.ReadLine());
+            var y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter x2 & y2 co-ordinates");
+            var x2 = Convert.ToDouble(Console.ReadLine());
+            var y2 = Convert.ToDouble(Console.ReadLine());
+
+            Double LengthOfaLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of a line is : " + LengthOfaLine);
+        }
+    }
+}
